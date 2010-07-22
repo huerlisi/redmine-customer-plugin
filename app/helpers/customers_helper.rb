9bbf3ccf11f5008fadde68fc894564c8853f0ba6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-22 08:39:07 +0200

    Fix global authorization.

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,9 +1,14 @@
 module CustomersHelper
   include ApplicationHelper
 
-  # This method would better be in main redmine...
+  # These methods would better be in main redmine...
+  # Display a link if user is authorized
+  def globally_authorize_for(controller, action)
+    User.current.allowed_to?({:controller => controller, :action => action, :global => true}, @project)
+  end
+
   # Display a link if user is authorized outside a project
   def link_to_if_globally_authorized(name, options = {}, html_options = nil, *parameters_for_method_reference)
-    link_to(name, options, html_options, *parameters_for_method_reference) if authorize_for(options[:controller] || params[:controller], options[:action], true)
+    link_to(name, options, html_options, *parameters_for_method_reference) if globally_authorize_for(options[:controller] || params[:controller], options[:action])
   end
 end
